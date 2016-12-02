6aaecd3e365129fd3f54ce266be6d6baf4adb439
hezio fernandes <hezio.fernandes@resultadosdigitais.com.br>
2016-12-02 13:33:17 -0200

    resolved another tests cases at RD

--- estacionamento/estacionamento_test.rb
+++ estacionamento/estacionamento_test.rb
@@ -3,52 +3,60 @@ require 'test/unit'
 
 class TestEstacionamento < Test::Unit::TestCase
 
-	def setup
-		@my_park = ParkingLot.new(500)
-		@a_car = Car.new('Gol', 'MMM1111')
+  def setup
+    @my_park = ParkingLot.new(500)
+    @a_car = Car.new('Gol', 'MMM1111')
 
-	end
+  end
 
-    def test_empty_parking_lot
-        assert_equal 500, @my_park.vacancies
+  def test_empty_parking_lot
+    assert_equal 500, @my_park.vacancies
+  end
+
+  def test_entrou_um_carro
+    @my_park.arrive_car(@a_car)
+    assert_equal 499, @my_park.vacancies
+  end
+
+  def test_entrou_um_carro_2
+    @my_park.arrive_car(@a_car)
+    assert_equal true, @my_park.is_car_in_parking_lot(@a_car)
+  end
+
+  def test_entrou_um_carro_3
+    assert_raise DuplicatedCarException do
+      @my_park.arrive_car(@a_car)
+      @my_park.arrive_car(@a_car)
     end
+  end
 
-    def test_entrou_um_carro
-    	@my_park.arrive_car(@a_car)
-    	assert_equal 499, @my_park.vacancies
+  def test_saiu_um_carro
+    @my_park.arrive_car(@a_car)
+    @my_park.leave_car(@a_car)
+    assert_equal 500, @my_park.vacancies
+  end
+
+  def test_nao_pode_sair_carro_que_nao_existe
+    assert_raise InvalidCarException do
+      another_car = Car.new('palio', 'THS0908')
+      @my_park.leave_car(another_car)
     end
+  end
 
-    def test_entrou_um_carro_2
-    	@my_park.arrive_car(@a_car)
-    	assert_equal true, @my_park.is_car_in_parking_lot(@a_car)
+  def test_estacionamento_lotado
+    # encher o estacionamento
+    another_park = ParkingLot.new(0)
+    assert_raise FullParkingLotException do
+      another_park.arrive_car(@a_car)
     end
-
-	def test_entrou_um_carro_3
-		assert_raise DuplicatedCarException do
-			@my_park.arrive_car(@a_car)
-			@my_park.arrive_car(@a_car)
-		end
-	end
-
-    def test_saiu_um_carro
-    	@my_park.arrive_car(@a_car)
-    	@my_park.leave_car(@a_car)
-    	assert_equal 500, @my_park.vacancies
-    end
-
-    # def test_nao_pode_entrar_carro_duplicado
-    # 	@my_park.arrive_car(@a_car)
-    # 	@my_park.arrive_car(@a_car)
-    # 	assert_raise DuplicatedCarException
-    # end
-
+  end
 end
 
-class 	TestCar < Test::Unit::TestCase
+class TestCar < Test::Unit::TestCase
 
-	def test_new_car
-		new_car = Car.new('Gol','MMM0909')
-		assert_equal 'Gol', new_car.model
-	end
+  def test_new_car
+    new_car = Car.new('Gol', 'MMM0909')
+    assert_equal 'Gol', new_car.model
+  end
 
 end
--- estacionamento/parking_lot.rb
+++ estacionamento/parking_lot.rb
@@ -14,74 +14,89 @@
 
 class ParkingLot
 
-    def vacancies
-        @vacancies
-    end
+  def vacancies
+    @vacancies
+  end
 
-    def car_list
-    	@car_list
-    end
+  def car_list
+    @car_list
+  end
 
-    def initialize(vacancies)
-        @car_list = Array.new(vacancies)
-        @vacancies = @car_list.length
-    end
+  def initialize(vacancies)
+    @car_list = Array.new(vacancies)
+    @vacancies = @car_list.length
+  end
 
-    def arrive_car(a_car)
-    	#@vacancies = @vacancies -1
-        if is_car_in_parking_lot(a_car) then
-          raise DuplicatedCarException
-        end
-          @car_list.push(a_car)
-            @vacancies -= 1
-     end
-
-    def leave_car(a_car)
-        # @car_list.remove(a_car)
-    	@vacancies += 1
+  def arrive_car(a_car)
+    #@vacancies = @vacancies -1
+    if is_car_in_parking_lot(a_car) then
+      raise DuplicatedCarException
     end
-
-    def is_car_in_parking_lot(a_car)
-        if @car_list.include?(a_car) then
-            return true
-        else
-            return false
-        end
+    if is_parking_lot_full() then
+      raise FullParkingLotException
     end
+    @car_list.push(a_car)
+    @vacancies -= 1
+  end
+
+  def leave_car(a_car)
+    unless is_car_in_parking_lot(a_car) then
+      raise InvalidCarException
+    end
+    @car_list.delete(a_car)
+    @vacancies += 1
+  end
+
+  def is_car_in_parking_lot(a_car)
+    if @car_list.include?(a_car) then
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_parking_lot_full
+    return @vacancies == 0
+  end
 
 end
 
 class DuplicatedCarException < StandardError
 end
 
+class InvalidCarException < StandardError
+end
+
+class FullParkingLotException < StandardError
+end
 
 class Car
 
-	# private String model;
-	def model
-		@model
-	end
+  # private String model;
+  def model
+    @model
+  end
 
-	#private String plate;
-	def plate
-		@plate
-	end
+  #private String plate;
+  def plate
+    @plate
+  end
 
-	#public String getModel() {
-	#	return this.model
-	#}
+  #public String getModel() {
+  #	return this.model
+  #}
 
-	#public Void setModel(String model) {
-	#	this,model = model;
-	#}
+  #public Void setModel(String model) {
+  #	this,model = model;
+  #}
 
-	#public Car(String model) {
-	#	setModel(modle);
-	#}
-	def initialize(model, plate)
-		@model = model
-		@plate = plate
-	end
+  #public Car(String model) {
+  #	setModel(modle);
+  #}
+  def initialize(model, plate)
+    @model = model
+    @plate = plate
+  end
 
 
 end
